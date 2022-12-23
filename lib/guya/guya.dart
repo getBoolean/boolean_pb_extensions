02e46f866749615bff383556dadfeaa34a783a3c
getBoolean <getboolean@gmail.com>
2022-12-23 01:19:17 -0600

    Update guya.dart

--- lib/guya/guya.dart
+++ lib/guya/guya.dart
@@ -14,7 +14,7 @@ const kGuyaDomain = 'https://guya.cubari.moe';
 const kGuyaApiBase = 'https://guya.cubari.moe/api';
 
 SourceInfo guyaInfo = SourceInfo(
-  version: '2.0.0',
+  version: '2.0.0-alpha.1',
   name: 'Guya (Dart)',
   icon: 'icon.png',
   author: 'getBoolean',
